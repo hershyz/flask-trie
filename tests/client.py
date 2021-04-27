ac907f92a2e7a8ac93f37e7f4b1b30ea2df650de
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-04-27 01:03:10 -0400

    Update client.py

--- tests/client.py
+++ tests/client.py
@@ -1,13 +1,32 @@
 import requests
 
-raw = input('enter args: ')
-args = raw.split(" ")
-url = ""
+print("Trie CLI (github.com/hershyz/trie) - type 'help' for help")
 
-for i in range(len(args)):
-    url += args[i]
-    if i < len(args) - 1:
-        url += "_"
+def help():
+    print("--- commands ---")
+    print("help: shows all available CLI commands")
+    print("add (keyword): adds a keyword to the trie if it does not already exist")
+    print("delete (keyword): deletes a keyword from the trie if it exists")
+    print("search (keyword): finds out if a keyword is contained within the trie")
+    print("display: displays the trie to the console")
 
-r = requests.get("http://127.0.0.1:5000/" + url)
-print(r.text)
+def commandLoop():
+
+    raw = input("> ")
+    args = raw.split(" ")
+    
+    if args[0] == "help":
+        help()
+    else:
+        url = ""
+        for i in range(len(args)):
+            url += args[i]
+            if i < len(args) - 1:
+                url += "_"
+
+        r = requests.get("http://127.0.0.1:5000/" + url)
+        print(r.text)
+
+    commandLoop()
+
+commandLoop()
