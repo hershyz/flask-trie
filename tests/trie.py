15149cbee4158f7e17ee443e61535fd6d358996d
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-04-27 22:51:20 -0400

    Update trie.py

--- tests/trie.py
+++ tests/trie.py
@@ -1,25 +1,27 @@
 class Node:
-    def __init__(self, data, is_end):
+    def __init__(self, data, end):
         self.children = []
-        self.end = is_end
         self.data = data
+        self.end = end
 
 class Trie:
 
     def __init__(self):
-        self.root = Node('*', False)
+        self.root = Node("*", False)
+        self.words = []
 
-    def display_client(self):
-        self.display(self.root)
+    def get_words(self):
+        self.words.clear()
+        self.get_words_helper(self.root, "")
 
-    def display(self, node):
-        children = node.children
-        s = ""
-        for i in range(len(children)):
-            s += children[i].data
-        print(s)
+    def get_words_helper(self, node, prefix):
+        
 
     def add_word(self, word):
+
+        if self.search(word):
+            return
+
         node = self.root
         chars = list(word)
         for i in range(len(chars)):
@@ -29,26 +31,34 @@ class Trie:
                 if child.data == target_char:
                     node = child
                     found = True
-                if not found:
-                    end = False
-                    if i == len(chars) - 1:
-                        end = True
-                    new_node = Node(target_char, end)
-                    node.children.append(new_node)
-                    node = new_node
-            if len(node.children) == 0:
-                    end = False
-                    if i == len(chars) - 1:
-                        end = True
-                    new_node = Node(target_char, end)
-                    node.children.append(new_node)
-                    node = new_node
+            if not found:
+                end = False
+                if i == len(chars) - 1:
+                    end = True
+                new_node = Node(target_char, end)
+                node.children.append(new_node)
+                node = new_node
+    
+    def search(self, word):
+        node = self.root
+        chars = list(word)
+        for i in range(len(chars)):
+            found = False
+            target_char = chars[i]
+            for child in node.children:
+                if child.data == target_char:
+                    found = True
+                    node = child
+            if not found:
+                return False
+        if node.end:
+            return True
+        return False
 
 
 
 trie = Trie()
 trie.add_word("apple")
-trie.add_word("car")
-trie.add_word("wab")
-trie.add_word("ack")
-trie.display_client()
+trie.add_word("apply")
+trie.add_word("boat")
+trie.add_word("bot")
