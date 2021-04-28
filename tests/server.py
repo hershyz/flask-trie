232e7ef71fb8b32a27c54680533471c05db67c37
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-04-28 07:52:43 -0400

    Update server.py

--- tests/server.py
+++ tests/server.py
@@ -9,17 +9,9 @@ def home():
 @app.route("/<raw>")
 def parameter(raw):
 
-    raw_string = str(raw)
-    args = raw_string.split('_')
-    result = ""
-    
-    for i in range(len(args)):
-        result += args[i]
-        if i < len(args) - 1:
-            result += ", "
-    
-    print(args)
-    return ("raw parameters: " + result)
+    raw_str = str(raw).lower()
+    args = raw_str.split('_')
+    return args[0] + " " + str(args)
 
 if __name__ == "__main__":
     app.run()
